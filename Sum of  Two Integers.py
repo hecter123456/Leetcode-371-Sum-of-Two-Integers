7f114597e820730625d9d3b7caf8ef5ad44e8762
hecter <hecter654321@gmail.com>
2018-02-24 20:05:18 +0800

    pass testcase: testMoveCase by using right shift

--- Sum of  Two Integers.py
+++ Sum of  Two Integers.py
@@ -6,17 +6,23 @@ class unitest(unittest.TestCase):
         b = 2
         Output = 3
         self.assertEqual(Solution().getSum(a,b),Output);
+    def testMoveCase(self):
+        a = 1
+        b = 1
+        Output = 2
+        self.assertEqual(Solution().getSum(a,b),Output);
 
 class Solution():
     def getSum(self, a, b):
         ans = 0
         move = 0
-        while(a != 0 or b != 0):
-            ans = ans << 1
-            ans |= (a%2) ^ (b%2) ^ move
+        k = 0
+        while(a != 0 or b != 0 or move != 0):
+            ans |= ((a%2) ^ (b%2) ^ move) << k
             move = ((a%2) & (b%2)) ^ ((a%2) & move) ^ (move & (b%2))
             a = a >> 1
             b = b >> 1
+            k += 1
         return ans
 
 if __name__ == '__main__':
